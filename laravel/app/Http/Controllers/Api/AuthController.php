086523e349727f9349e8f3b5a044a4bcfae10e2e
Dzyfhuba <hafidz21ub@gmail.com>
2023-08-31 18:31:53 +0700

    add api login

---
+++ laravel/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return response($validator->getMessageBag(), 400);
+        }
+
+        $user = User::create([
+            ...$request->all(),
+            'role' => 'user'
+        ]);
+
+        $token = $user->createToken('login');
+
+        return response([
+            'message' => 'register success',
+            'token' => $token->plainTextToken
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response($validator->getMessageBag(), 400);
+        }
+
+        $user = User::query()->where('email', $request->email)->first();
+
+        if (!Auth::attempt($request->all()))
+            return response([
+                'message' => 'invalid'
+            ], 400);
+
+        return response([
+            'message' => 'login success',
+            'token' => $user->createToken('login')->plainTextToken
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response([
+            'message' => 'logout success'
+        ]);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/posts', function() {
+        return response('test');
+    });
+});
