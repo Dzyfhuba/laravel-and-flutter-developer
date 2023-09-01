135401bb673e356d1ecab08c78c64417d292ee73
Dzyfhuba <hafidz21ub@gmail.com>
2023-09-02 01:42:44 +0700

    update login page

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,74 +1,105 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const LoginPage());
+  runApp(const MyApp());
 }
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Login',
-      home: Scaffold(
-        body: LoginForm(),
-      ),
+      title: 'State Example',
+      home: LoginPage(title: 'State Example'),
     );
   }
 }
 
-class LoginForm extends StatelessWidget {
-  const LoginForm({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => LoginForm();
+}
+
+class LoginForm extends State<LoginPage> {
+  String _email = "";
+  String _password = "";
+  final _form = GlobalKey<FormState>();
+  bool _isValid = false;
+
+  void _saveForm() {
+    setState(() {
+      _isValid = _form.currentState!.validate();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        const Center(
-          child: Text("Login Page",
-              textScaleFactor: 3,
-              style: TextStyle(fontWeight: FontWeight.bold)),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Email',
-            ),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        // the Form here
+        child: Form(
+          key: _form,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Center(
+                child: Text("Login Page",
+                    textScaleFactor: 2,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline)),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  // Check if this field is empty
+                  if (value == null || value.isEmpty) {
+                    return 'This field is required';
+                  }
+
+                  // using regular expression
+                  if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                    return "Please enter a valid email address";
+                  }
+
+                  // the email is valid
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Password'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  // Check if this field is empty
+                  if (value == null || value.isEmpty) {
+                    return 'This field is required';
+                  }
+
+                  // the email is valid
+                  return null;
+                },
+              ),
+              Container(
+                alignment: Alignment.topRight,
+                child: TextButton(
+                    onPressed: _saveForm,
+                    style: const ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(
+                            Color.fromRGBO(230, 138, 0, 1))),
+                    child: const Text('Login', style: TextStyle(color: Colors.black),)),
+              ),
+            ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Password',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Align(
-            alignment: Alignment.topRight,
-            child: SizedBox(
-            height: 44,
-            width: 100,
-            child: TextButton(
-              onPressed: () {},
-              style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll(Color.fromRGBO(232, 132, 26, 1))),
-              child: const Text('Login',
-                  style: TextStyle(color: Color.fromRGBO(0, 0, 0, 1)), textScaleFactor: 1.4,),
-            ),
-          ),
-          ),
-        )
-      ],
+      ),
     );
   }
 }
