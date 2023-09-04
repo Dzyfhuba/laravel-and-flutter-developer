055103864c930e840dee8cc83e058c3f8ab73ffa
Dzyfhuba <hafidz21ub@gmail.com>
2023-09-04 22:29:49 +0700

    add new post page

--- flutter_app/lib/post_create.dart
+++ flutter_app/lib/post_create.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' hide Text;
+import 'package:intl/intl.dart';
+// import 'package:flutter_quill/flutter_quill.dart';
 
 class PostCreate extends StatefulWidget {
   const PostCreate({super.key});
@@ -8,24 +11,94 @@ class PostCreate extends StatefulWidget {
 }
 
 class PostCreateState extends State<PostCreate> {
+  TextEditingController titleField = TextEditingController();
+  QuillController contentField = QuillController.basic();
+  bool statusField = false;
+  String _publishedDateField = '';
+  final _form = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-        Text('data'),
-      ],
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xFFE68A00),
+        onPressed: () {},
+        child: const Icon(Icons.save),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _form,
+          child: Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: titleField,
+                  decoration: const InputDecoration(
+                    labelText: 'Title',
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Text('Status: '),
+                    Switch(
+                      activeColor: Color(0xFFE68A00),
+                      value: statusField,
+                      onChanged: (value) {
+                        setState(() {
+                          statusField = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                // CalendarDatePicker(
+                //   initialDate: DateTime.now(),
+                //   firstDate: DateTime.now(),
+                //   lastDate: DateTime.now(),
+                //   onDateChanged: (value) {},
+                // ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                    style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Color.fromRGBO(230, 138, 0, 0.531)),
+                    ),
+                    onPressed: () async {
+                      var date = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime.parse('2099-12-31'),
+                      );
+                      if (date == null) return;
+                      setState(() {
+                        _publishedDateField =
+                            DateFormat('y-MM-dd').format(date!);
+                      });
+                    },
+                    child: Text('Published Date: $_publishedDateField',
+                        style: const TextStyle(color: Colors.black)),
+                  ),
+                ),
+
+                QuillToolbar.basic(
+                  controller: contentField,
+                ),
+                Expanded(
+                  child: QuillEditor.basic(
+                    controller: contentField,
+                    readOnly: false,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
